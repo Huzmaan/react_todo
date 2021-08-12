04f3bb372e79fae520ff07df617efb5fc1feabc4
Huzmaan <princehuzmaanpasta@gmail.com>
2021-08-13 00:09:18 +0500

    working main component

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
 
-function Main ({}){
+
+
+
+class Main extends React.Component{
+
+  constructor(){
+    super()
+    this.state={
+     todos:[ "Ghous","Ahmed","Umair"],
+     value:""
+    }
+  }
+  add_todo=()=>{
+    this.state.todos.push(this.state.value)
+  }
+  render(){
+    let {todos,value} = this.state;
     return(
   <div>
-      <p>
-          
-      </p>
+     <input  value={value} onChange={(e) =>this.setState({value:e.target.value})} type="text" placeholder="Enter your Todo" />
+     <button onClick={this.add_todo}>Add Todo</button>
+     <ul>
+       {todos.map((v,i)=>{
+        return <li  key={i}>{v}</li>
+       })}
+     </ul>
   </div>
     )
-}
+     } }
 export default Main
